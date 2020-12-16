732510e2d2bc88a883b1f311c4aff256c850c45d
İbrahim Bali <balii@pm.me>
2020-12-16 20:20:40 +0300

    refactor(ReadTablenamesFromConfig): to return services for easier chaining

--- Dapper.Contrib.Extensions.Tablename/Dapper.Contrib.Extensions.Tablename/Extensions.cs
+++ Dapper.Contrib.Extensions.Tablename/Dapper.Contrib.Extensions.Tablename/Extensions.cs
@@ -8,11 +8,12 @@ namespace Dapper.Contrib.Extensions.Tablename
     public static class TablenameExtensions
     {
         private static TablenameConfig _config;
-        public static void ReadTablenamesFromConfig(this IServiceCollection services, IConfigurationSection configSection)
+        public static IServiceCollection ReadTablenamesFromConfig(this IServiceCollection services, IConfigurationSection configSection)
         {
             services.Configure<TablenameConfig>(configSection);
             _config = configSection.Get<TablenameConfig>();
             SqlMapperExtensions.TableNameMapper = TableName;
+            return services;
         }
 
         private static string TableName(Type type) => _config.TableNames[type.FullName].Replace("`", "");
